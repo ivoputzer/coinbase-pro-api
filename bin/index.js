a9fcb6deedff42fae195d40798f594f9ab77878b
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2018-11-27 14:35:12 +0100

    updates available requests

--- bin/index.js
+++ bin/index.js
@@ -4,12 +4,12 @@ require('console.table')
 
 const [cmd, ...args] = process.argv.slice(2)
 
-requestFor(cmd)(args)
+dataFor(cmd)(args)
   .then(console.table)
   .catch(console.error)
 
-function requestFor (command, api = require('..')) {
-  const toCamelCase = s => s.replace(/(\-\w)/g, m => m[1].toUpperCase())
+function dataFor (command, api = require('..')) {
+  const toCamelCase = s => s.replace(/(-\w)/g, m => m[1].toUpperCase())
   const method = toCamelCase(`get-${command}`)
   return api[method] || Promise.resolve.bind(Promise, `command: \`${command}\` not available.`)
 }
