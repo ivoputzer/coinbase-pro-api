c941f91b1af7cca24834321e062cb9830fa24aab
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2019-01-02 22:47:35 +0100

    upgrades to node 10+ assertions

--- test/lib/client.js
+++ test/lib/client.js
@@ -42,123 +42,36 @@ test('lib/client', () => {
         }
       }
     })
-
-    // test('returns signature headers', () => {
-    //   const headers =
-
-    //   const method = 'get'
-    //   const path = '/'
-
-    //   signatureFor({method: get})
-    // })
   })
 
-  // test('.headersFor', () => {
-  //   const {headersFor} = require('../../lib/client')
+  test('.headersFor', () => {
+    const { headersFor } = require('../../lib/client')
 
-  //   test('is callable', () => {
-  //     deepStrictEqual(typeof headersFor, 'function')
-  //   })
-  // })
+    test('is callable', () => {
+      deepStrictEqual(typeof headersFor, 'function')
+    })
+  })
 
-  // test('.headersFor', () => {
-  //   const {headersFor} = require('../../lib/client')
+  test('.hostnameFor', () => {
+    const { hostnameFor } = require('../../lib/client')
 
-  //   test('is callable', () => {
-  //     deepStrictEqual(typeof headersFor, 'function')
-  //   })
-  // })
+    test('is callable', () => {
+      deepStrictEqual(typeof hostnameFor, 'function')
+    })
+  })
 
-  // test('.hostnameFor', () => {
-  //   const {hostnameFor} = require('../../lib/client')
+  test('.requestFor', () => {
+    const { request: requestFor } = require('../../lib/client')
 
-  //   test('is callable', () => {
-  //     deepStrictEqual(typeof hostnameFor, 'function')
-  //   })
-  // })
+    test('is callable', () => {
+      deepStrictEqual(typeof requestFor, 'function')
+    })
+  })
 
-  // test('.requestFor', () => {
-  //   const {request: requestFor} = require('../../lib/client')
-
-  //   test('is callable', () => {
-  //     deepStrictEqual(typeof requestFor, 'function')
-  //   })
-  // })
-
-  // test('.toString', () => {
-  //   const {toString} = require('../../lib/client')
-  //   test('is callable', () => {
-  //     deepStrictEqual(typeof toString, 'function')
-  //   })
-  // })
+  test('.toString', () => {
+    const { toString } = require('../../lib/client')
+    test('is callable', () => {
+      deepStrictEqual(typeof toString, 'function')
+    })
+  })
 })
-
-// const reqheaders = {
-//   'Content-Type': 'application/json',
-//   'Accept': 'application/json',
-//   'User-Agent': process.env.npm_package_name + ' ' + process.env.npm_package_version
-// }
-
-// test('.request', () => {
-//   const {request} = require('..')
-
-//   test('is of type function', () => {
-//     equal(typeof request, 'function')
-//   })
-
-//   test('makes standard `https.request` promise compatibile', () => {
-//     const defaultOptions = {
-//       protocol: 'https:',
-//       slashes: true,
-//       hostname: 'api.pro.coinbase.com',
-//       path: '/time?option=true',
-//       headers: {
-//         'User-Agent': `${process.env.npm_package_name}  ${process.env.npm_package_version}`
-//       }
-//     }
-
-//     test('supports `head` requests', Function.prototype)
-
-//     test('supports `options` requests', Function.prototype)
-
-//     test('supports `post` requests', Function.prototype)
-
-//     test('supports `put` requests', Function.prototype)
-
-//     test('supports `delete` requests', Function.prototype)
-
-//     test('supports `get` requests', (done) => {
-//       const options = {...defaultOptions, method: 'get'}
-//       const https = {
-//         request: stub()
-//           .yields({statusCode: 200, headers: {}})
-//           .returns({on: Function.prototype})
-//       }
-
-//       nock('https://api.pro.coinbase.com')
-//         .get('/time')
-//         .query({option: true})
-//         .reply(200)
-
-//       request(options, {}, https)
-//         .then(res => {
-//           equal(res.statusCode, 200)
-//           ok(https.request.calledWith(options), '`https.request` should have been called')
-//         })
-//         .then(done)
-//         .catch(done)
-//     })
-//   })
-
-//   test('handles standard `https.request` errors', (done) => {
-//     const options = {protocol: 'https:', method: 'post', slashes: true, hostname: 'localhost', path: '/', headers: {}}
-//     const https = {request: stub().returns({on: stub().yields({error: true})})}
-
-//     request(options, {}, https)
-//       .then(done)
-//       .catch(err => {
-//         deepStrictEqual(err, {error: true})
-//         done()
-//       })
-//   })
-// })
